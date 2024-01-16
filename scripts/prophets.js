9c92d80d6316d91fa6cc432dafff4a2d0b4241ca
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-15 20:22:23 -0600

    more comfortable fetch request

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,20 +1,26 @@
 const url = "https://lenorastevens.github.io/wdd330/data/prophets.json";
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            data.prophets.forEach(prophet => {
-                const template = document.getElementById('prophet-card');
-                const card = document.importNode(template.content, true);
+    async function getProphetData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.table(data.prophets);
+        showProphets(data.prophets);
+    }
+        
+    const showProphets = (prophets) => {
+        prophets.forEach((prophet) => {
+            const template = document.getElementById('prophet-card');
+            const card = document.importNode(template.content, true);
 
-                card.querySelector('h2').textContent = `${prophet.name} ${prophet.lastname}`;
-                card.querySelector('p:nth-of-type(1)').textContent = `Birthdate: ${prophet.birthdate}`;
-                card.querySelector('p:nth-of-type(2)').textContent = `Death: ${prophet.death}`;
-                card.querySelector('.profile').src = prophet.imageurl;
+            card.querySelector('h2').textContent = `${prophet.name} ${prophet.lastname}`;
+            card.querySelector('p:nth-of-type(1)').textContent = `Birthdate: ${prophet.birthdate}`;
+            card.querySelector('p:nth-of-type(2)').textContent = `Death: ${prophet.death}`;
+            card.querySelector('.profile').src = prophet.imageurl;
 
-                document.body.appendChild(card);
-            });
-        })
-        .catch(error => console.error('Error fetching JSon:', error));
+            document.body.appendChild(card);
+        });
+    }
+
+    getProphetData()
 });
