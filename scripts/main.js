932e88100a4a3240352a032912c46d65f325baee
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-24 20:50:47 -0600

    working on forms practice

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const prophetSection = document.getElementById('prophet');
     const pokeapi = document.getElementById('pokeapi');
     const cpForm = document.getElementById('cp-form');
+    const submitForm = document.getElementById('submit-form');
 
     prophetSection.addEventListener('click', () => {
         window.location.href = 'prophets.html';
@@ -11,5 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     cpForm.addEventListener('click', () => {
         window.location.href = 'https://codepen.io/Lenora-Stevens/pen/Babdobp';
+    });
+    submitForm.addEventListener('click', () => {
+        window.location.href = '../form-data/form.html';
     });
 });
