0c255488a1cd1a6ea328197660968ac98b211bfb
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-24 18:37:17 -0600

    api and form work

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const prophetSection = document.getElementById('prophet');
+    const pokeapi = document.getElementById('pokeapi');
+    const cpForm = document.getElementById('cp-form');
 
     prophetSection.addEventListener('click', () => {
         window.location.href = 'prophets.html';
     });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const prophetSection = document.getElementById('pokeapi');
-
-    prophetSection.addEventListener('click', () => {
+    pokeapi.addEventListener('click', () => {
         window.location.href = '../poke-api/poke.html';
+    });
+    cpForm.addEventListener('click', () => {
+        window.location.href = 'https://codepen.io/Lenora-Stevens/pen/Babdobp';
     });
 });
