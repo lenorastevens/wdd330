acbd72585f26f89f26565e92e33f49680a8d0104
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-29 18:33:25 -0600

    forgot to push form data work

--- form-data/scripts/formdata.js
+++ form-data/scripts/formdata.js
@@ -1,48 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document
-      .getElementById('myForm')
-      .addEventListener('submit', handleForm);
-  });
-
-  function handleForm(ev) {
-    ev.preventDefault(); //stop the page reloading
-    //console.dir(ev.target);
-    let myForm = ev.target;
-    let fd = new FormData(myForm);
-
-    //add more things that were not in the form
-    fd.append('api-key', 'myApiKey');
-
-    //look at all the contents
-    for (let key of fd.keys()) {
-      console.log(key, fd.get(key));
-    }
-    let json = convertFD2JSON(fd);
-
-    //send the request with the formdata
-    let url = 'http://localhost:3000/';
-    let h = new Headers();
-    h.append('Content-type', 'application/json');
-
-    let req = new Request(url, {
-      headers: h,
-      body: json,
-      method: 'POST',
-    });
-    //console.log(req);
-    fetch(req)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('Response from server');
-        console.log(data);
-      })
-      .catch(console.warn);
-  }
-
-  function convertFD2JSON(formData) {
-    let obj = {};
+  const formElem = document.getElementById('myForm');
+  formElem.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(formElem);
+    formData.append('submitted', new Date());
     for (let key of formData.keys()) {
-      obj[key] = formData.get(key);
+      console.log(key, formData.get(key));
     }
-    return JSON.stringify(obj);
-  }
+  });
+});
